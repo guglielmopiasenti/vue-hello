84d14e43e078f15a8633292d05c81cb26877abe9
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-06-16 15:39:29 +0200

    vue hello completed

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const app = createApp({
   data() {
     return {
       // data definition
+      title: "Hello Vue",
+      pic: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpositivethinking.tech%2Finsights%2Fvue-js-as-an-enterprise-solution%2F&psig=AOvVaw3n3xh9yUKeAOMTqowD7wrn&ust=1687008363564000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCLC0qYTyx_8CFQAAAAAdAAAAABAE",
     };
   },
 });
